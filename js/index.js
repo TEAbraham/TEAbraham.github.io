c9784905d8a578f8c9f89fca65d3bf9e4c27348f
Thomas Abraham <thomas.e.abraham@gmail.com>
2022-10-19 18:44:31 -0400

    radio button init

--- js/index.js
+++ js/index.js
@@ -10,3 +10,43 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+var ALL = document.getElementById('sc-1-1-1');
+var BIO = document.getElementById('sc-1-1-2');
+var NLP = document.getElementById('sc-1-1-3');
+var FIN = document.getElementById('sc-1-1-4');
+
+ALL.onclick = function() {
+  for (let el of document.querySelectorAll('.portfolio__item')){
+    if ( el.style.visibility == 'collapse') {
+      el.style.visibility = 'visible';
+    }
+  }
+};
+
+BIO.onclick = function () {
+  for (let el of document.querySelectorAll('.portfolio__item') )el.style.visibility = 'collapse';
+  for (let el of document.querySelectorAll('.bio')){
+    if ( el.style.visibility == 'collapse') {
+      el.style.visibility = 'visible';
+    }
+  }
+};
+
+NLP.onclick = function () {
+  for (let el of document.querySelectorAll('.portfolio__item') )el.style.visibility = 'collapse';
+  for (let el of document.querySelectorAll('.nlp')){
+    if ( el.style.visibility == 'collapse') {
+      el.style.visibility = 'visible';
+    }
+  }
+};
+
+FIN.onclick = function () {
+  for (let el of document.querySelectorAll('.portfolio__item') )el.style.visibility = 'collapse';
+  for (let el of document.querySelectorAll('.fin')){
+    if ( el.style.visibility == 'collapse') {
+      el.style.visibility = 'visible';
+    }
+  }
+};
