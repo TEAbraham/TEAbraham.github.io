3d1e0f105bb25312549a9face2802b5796ede2ca
Thomas Abraham <thomas.e.abraham@gmail.com>
2024-12-16 14:39:53 -0500

    interactive regression init

---
+++ ap_stats/interactive_regression/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>Interactive Linear Regression</h1>
+    <LinearRegression />
+  </div>
+</template>
+
+<script>
+import LinearRegression from "./components/LinearRegression";
+
+export default {
+  components: {
+    LinearRegression,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  margin: 20px;
+}
+</style>
